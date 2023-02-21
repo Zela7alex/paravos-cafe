4d56be7700dfd0a3884bd6d61a8d04a643c8cdac
Alexandra <alexandra@Alexandras-MacBook-Pro.local>
2023-02-21 12:09:07 -0700

    added public folder to server for deployment

--- vue-app/vue.config.js
+++ vue-app/vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require("@vue/cli-service");
+// eslint-disable-next-line no-unused-vars
+const path = require("path");
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  outputDir: path.resolve(__dirname, "../api/public"),
+  devServer: {
+    proxy: {
+      target: "http://localhost:8080",
+    },
+  },
 });
