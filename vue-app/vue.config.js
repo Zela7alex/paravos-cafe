4483b198a10d16d7d046ad9c69c6d290d260dc5b
Alexandra <alexandra@Alexandras-MacBook-Pro.local>
2023-02-21 12:19:23 -0700

    deploy test

--- vue-app/vue.config.js
+++ vue-app/vue.config.js
@@ -4,10 +4,13 @@ const path = require("path");
 
 module.exports = defineConfig({
   transpileDependencies: true,
+});
+
+module.exports = {
   outputDir: path.resolve(__dirname, "../api/public"),
   devServer: {
     proxy: {
       target: "http://localhost:8080",
     },
   },
-});
+};
